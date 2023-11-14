083f223cc0e39b5a7de603e22a7d04c8034f5f08
francomatuz <francomatuz@gmail.com>
2023-11-14 20:31:39 -0300

    Initial commit

---
+++ src/main/java/com/egg/salud_webapp/SaludWebappApplication.java
@@ -0,0 +1,13 @@
+package com.egg.salud_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludWebappApplication.class, args);
+	}
+
+}
