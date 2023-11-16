765c1defc8e1ad3c35f50deae2b16c22dbfa9589
francomatuz <francomatuz@gmail.com>
2023-11-16 20:22:44 -0300

    Cambios a portalcontrolador

--- src/main/java/com/egg/salud_webapp/controladores/PortalControlador.java
+++ src/main/java/com/egg/salud_webapp/controladores/PortalControlador.java
@@ -1,6 +1,6 @@
-
 package com.egg.salud_webapp.controladores;
 
+import com.egg.salud_webapp.enumeraciones.Rol;
 import com.egg.salud_webapp.excepciones.MiException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,26 +13,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
-    
-    
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index.html";
     }
-    
- @PostMapping("/registrar")
-    public  registrar(string tipoUsuario )   {//tipo usuario va a ser un enum acordado con el front
-       //recibe enum que diga si es profesional o paciente
-       if (tipoUsuario == enumPaciente) {
-           retrun "registrarpaciente.html"
-       }else return "registrarprofesional.html"
-         
-     }
-    @PostMapping ("/registrar/paciente")
-    public registrarpaciente ()
 
-@PostMapping ("/registrar/profesional")
-    public registrarprofesional ()
-            
+    @PostMapping("/registrar")
+    public String registrar(Rol rol) {//tipo usuario va a ser un enum acordado con el front
+        //recibe enum que diga si es profesional o paciente
+        if (rol == Rol.PACIENTE) {
+            return "registrarpaciente.html";
+        } else if (rol == Rol.PROFESIONAL) {
+
+            return "registrarprofesional.html";
+        } else{
+            // Ver la posibilidad de otro caso o lanzar una excepcion si es necesario
+            return "error.html";
+        }
+
+        
+        
+        
+        //falta logica
+        @PostMapping("/registrar/paciente")
+        public String registrarpaciente(){
+        
+        
+        return "login.html";
+        }
+
+        @PostMapping("/registrar/profesional")
+        public String registrarprofesional(){
+        
+        return "solicitud.html";
+        }
+
+    }
 }
