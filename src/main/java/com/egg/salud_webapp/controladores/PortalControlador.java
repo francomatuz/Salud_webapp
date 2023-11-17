9d4ddc0af38f4839b44d52759e669d0409423399
Fedestack <124637861+Fedestack@users.noreply.github.com>
2023-11-16 21:29:10 -0300

    modificaciones

--- src/main/java/com/egg/salud_webapp/controladores/PortalControlador.java
+++ src/main/java/com/egg/salud_webapp/controladores/PortalControlador.java
@@ -2,6 +2,7 @@ package com.egg.salud_webapp.controladores;
 
 import com.egg.salud_webapp.enumeraciones.Rol;
 import com.egg.salud_webapp.excepciones.MiException;
+import java.time.LocalDate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -19,7 +21,7 @@ public class PortalControlador {
         return "index.html";
     }
 
-    @PostMapping("/registrar")
+    @GetMapping("/registrar")
     public String registrar(Rol rol) {//tipo usuario va a ser un enum acordado con el front
         //recibe enum que diga si es profesional o paciente
         if (rol == Rol.PACIENTE) {
@@ -29,19 +31,27 @@ public class PortalControlador {
             return "registrarprofesional.html";
         } else{
             // Ver la posibilidad de otro caso o lanzar una excepcion si es necesario
-            return "error.html";
+            throw new MiException ("Rol no valido"+rol);
+                    
         }
 
         
-        
-        
         //falta logica
         @PostMapping("/registrar/paciente")
-        public String registrarpaciente(){
+        public String registrarpaciente(@RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String dni, @RequestParam LocalDate fecha_nac, @RequestParam ObraSocial obraSocial, @RequestParam GeneroEnum genero @RequestParam String password, @RequestParam String password2){
         
+             try { 
+                 pacienteServicio.
+            
+        } catch (Exception e) {
+        }
         
         return "login.html";
         }
+        
+        
+        
+        
 
         @PostMapping("/registrar/profesional")
         public String registrarprofesional(){
