313636d00115ab79267ce8fe3525255adc46015f
Camilo Forero <124638099+Camilo-Forero04@users.noreply.github.com>
2023-11-24 18:19:16 -0500

    dashboard eliminar

--- src/main/java/com/egg/salud_webapp/controladores/PerfilPacienteControlador.java
+++ src/main/java/com/egg/salud_webapp/controladores/PerfilPacienteControlador.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,7 +31,28 @@ public class PerfilPacienteControlador {
     public String dashboard() {
         return "dashboardpaciente.html";
     }
+    //ELIMINAR
+    @GetMapping("/eliminar")
+    public String eliminar(HttpSession session, ModelMap modelo) throws MiException {
+        
+        Paciente pacienteLogueado = (Paciente) session.getAttribute("usuariosession");
 
+        try {
+            
+            pacienteServicio.eliminar(pacienteLogueado.getId());
+
+
+            return "index.html";
+
+        } catch (MiException ex) {
+
+            modelo.put("error", ex.getMessage());
+
+            return "redirect:../dashboardpaciente.html";
+        }
+
+    }
+    
     @GetMapping("/actualizar")
     public String mostrarFormulario(ModelMap modelo, HttpSession session) {
         
